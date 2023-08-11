cc3a40f6fc4e5cf90ddce8261359953129895dc8
Utyak <amnesia_airone@mail.ru>
2023-08-11 20:46:47 +0300

    moved getting the progression to a separate function that does not change the value in the array to ..

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -7,23 +7,23 @@ const MAX_START = 15;
 const MIN_STEP = 1;
 const MAX_STEP = 10;
 
+const generateProgression = (startValue, step, length) => new Array(length)
+  .fill(startValue)
+  .map((num, index) => num + step * index);
+
 const game = {
   description: 'What number is missing in the progression?',
   round: () => {
     const length = getRandomInt(MIN_LENGTH, MAX_LENGTH);
-    const start = getRandomInt(MIN_START, MAX_START);
+    const startValue = getRandomInt(MIN_START, MAX_START);
     const step = getRandomInt(MIN_STEP, MAX_STEP);
     const hiddenIndex = getRandomInt(0, length - 1);
 
-    const progression = new Array(length)
-      .fill(start)
-      .map((num, index) => {
-        if (index === hiddenIndex) return '..';
-        return num + step * index;
-      });
+    const progression = generateProgression(startValue, step, length);
+    const correctAnswer = progression[hiddenIndex].toString();
+    progression[hiddenIndex] = '..';
 
     const question = progression.join(' ');
-    const correctAnswer = (start + step * hiddenIndex).toString();
 
     return {
       question,
